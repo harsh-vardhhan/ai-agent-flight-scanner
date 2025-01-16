d68d7882b83a8393a8e3a9708354a36de4221a43
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-01-16 15:01:12 +0530

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -1,14 +1,5 @@
 import json
 import sqlite3
-from datetime import datetime
-
-def convert_date_to_iso(date_str):
-    if date_str:
-        day, month = date_str.split()
-        day = int(day)
-        month = datetime.strptime(month, '%B').month
-        return f"{datetime.now().year:04d}-{month:02d}-{day:02d}"
-    return None
 
 def json_to_sqlite(json_file, sqlite_file):
     with open(json_file, 'r') as file:
@@ -27,13 +18,9 @@ def json_to_sqlite(json_file, sqlite_file):
                     )''')
 
     for item in data:
-        iso_date = convert_date_to_iso(item['date'])
-        price_str = item['price_inr'].replace('₹', '').replace(',', '')
-        price_int = int(price_str)
-
         cursor.execute('''INSERT INTO flights (date, origin, destination, price_inr, flightType)
                            VALUES (?, ?, ?, ?, ?)''',
-                       (iso_date, item['origin'], item['destination'], price_int, item['flightType']))
+                       (item['date'], item['origin'], item['destination'], item['price_inr'], item['flightType']))
 
     conn.commit()
     conn.close()
