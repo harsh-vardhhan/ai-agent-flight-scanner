7462526e86792e5aae31aa83c1a1da7acb47ddd5
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-01-24 20:48:06 +0530

    removed Groq

--- app/llm.py
+++ app/llm.py
@@ -1,16 +1,9 @@
 import os
-from langchain_groq import ChatGroq
 from langchain_ollama import ChatOllama
 from langchain_openai.chat_models.base import BaseChatOpenAI
 
 def get_llm(model_name, platform_name="OLLAMA"):
-    if platform_name == "GROQ":
-        return ChatGroq(
-            temperature=1,
-            model=model_name,
-            groq_api_key=os.environ["GROQ_API_KEY"]
-        )
-    elif platform_name == "OLLAMA":
+    if platform_name == "OLLAMA":
         return ChatOllama(
             model=model_name,
             temperature=0.2,
