ee7605680fea3baa2b12fab73b4b8b3866a08547
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-02-03 16:34:57 +0530

    Update llm.py

--- app/llm.py
+++ app/llm.py
@@ -2,6 +2,7 @@ import os
 from langchain_ollama import ChatOllama
 from langchain_groq import ChatGroq
 from langchain_openai.chat_models.base import BaseChatOpenAI
+from dotenv import load_dotenv
 
 def get_llm(model_name, platform_name="OLLAMA"):
     if platform_name == "OLLAMA":
@@ -13,12 +14,12 @@ def get_llm(model_name, platform_name="OLLAMA"):
         return ChatGroq(
             temperature=1,
             model=model_name,
-            groq_api_key=os.environ["GROQ_API_KEY"]
+            groq_api_key=os.getenv("GROQ_API_KEY")
         )
     elif platform_name == 'DEEPSEEK':
         return BaseChatOpenAI(
             model=model_name,
-            openai_api_key=os.environ["OPENAI_API_KEY"],
+            openai_api_key=os.getenv("DEEPSEEK_API_KEY"),
             openai_api_base='https://api.deepseek.com',
             max_tokens=1024
         )
